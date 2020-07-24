b5f32516b727594273c3e94e335e08a7842263dd
Sean Patterson <slpatterson97@gmail.com>
2020-07-23 21:35:04 -0600

    yarn fix2

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
 return (
 <header className="header">
 <div className="container clearfix">                  
-    <img className="profile-image img-fluid float-left rounded-circle" src="assets/images/profile.png" alt="profile image" />
+    <img className="profile-image img-fluid float-left rounded-circle" src="assets/images/profile.png"/>
     <div className="profile-content float-left">
         <h1 className="name">Sean Patterson</h1>
         <h2 className="desc">Software Developer</h2>   
