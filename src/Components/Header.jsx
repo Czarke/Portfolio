ae60842cba9e5304a8ebc59a0931e9121ef2aa73
Sean Patterson <slpatterson97@gmail.com>
2021-02-15 18:50:32 -0700

    picture change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
 return (
 <header className="header">
 <div className="container clearfix">                  
-    <img className="profile-image img-fluid float-left rounded-circle" src="assets/images/profile.png" alt="Profile"/>
+    <img className="profile-image img-fluid float-left rounded-circle" src="assets/images/profile.jpg" alt="Profile"/>
     <div className="profile-content float-left">
         <h1 className="name">Sean Patterson</h1>
         <h2 className="desc">Software Developer</h2>   
